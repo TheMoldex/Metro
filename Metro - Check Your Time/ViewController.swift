18b811b810e19fb55f2ddad550b419fd465f27a4
Denis Stashkov <themoldex@gmail.com>
2023-09-26 00:15:36 +0300

    add month bar , but cant set norm size

--- Metro - Check Your Time/ViewController.swift
+++ Metro - Check Your Time/ViewController.swift
@@ -8,12 +8,26 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var calendar: UIButton!
+    @IBOutlet weak var now: UIButton!
+    @IBOutlet weak var norma: UIButton!
+    
+    @IBOutlet weak var prevButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        madeCornerRadius()
     }
-
+    
+    private func madeCornerRadius() {
+        calendar.layer.cornerRadius = 20
+        now.layer.cornerRadius = 20
+        norma.layer.cornerRadius = 20
+        prevButton.layer.cornerRadius = prevButton.frame.width / 2
+        nextButton.layer.cornerRadius = nextButton.frame.width / 2
+    }
 
 }
 
