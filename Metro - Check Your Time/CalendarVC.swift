be5af83ad24d0ffd051bd26a453debba6dbe9751
Denis Stashkov <themoldex@gmail.com>
2023-10-01 23:02:31 +0300

    add 1 view and rename

---
+++ Metro - Check Your Time/CalendarVC.swift
@@ -0,0 +1,29 @@
+//
+//  CalendarVC.swift
+//  Metro - Check Your Time
+//
+//  Created by Денис Сташков on 1.10.2023.
+//
+
+import UIKit
+
+class CalendarVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
